621995720791e5b965efd4e94b650b5995763ddd
Ruben <rubenbaghajyan@gmail.com>
2026-02-07 20:44:00 +0400

    feat: filter TopProjects to only display projects marked as top

--- src/components/sections/home/TopProjects.jsx
+++ src/components/sections/home/TopProjects.jsx
@@ -2,9 +2,11 @@ import { ProjectGrid } from '../../home';
 import DesigningSuccess from './DesigningSuccess';
 
 const TopProjects = ({ projects }) => {
+  const topProjects = projects.filter((project) => project.top === true);
+
   return (
     <div className="flex flex-col gap-16">
-      <ProjectGrid projects={projects} />
+      <ProjectGrid projects={topProjects} />
       <DesigningSuccess />
     </div>
   );
